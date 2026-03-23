646c025c4a6dd7034d7d075efe1eaed9449a0173
Franciscohpina <2124300344@soy.utj.edu.mx>
2026-03-22 19:49:02 -0600

    Change database connection settings in database.js
    Updated database connection details to use specific credentials and IP address.

--- config/database.js
+++ config/database.js
@@ -2,11 +2,22 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // Crear pool de conexiones (mejor rendimiento)
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: '198.59.144.130', // Tu Shared IP de cPanel
+  user: 'estet1caleos_proyecto', // El usuario que creaste en el Wizard
+  password: 'sistemaleos12345!', 
+  database: 'estet1caleos_estetica_db', // Nombre completo de la BD
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -25,4 +36,6 @@ pool.getConnection((err, connection) => {
   }
 });
 
-module.exports = promisePool;
+//module.exports = promisePool;
+
+module.exports = pool.promise();
